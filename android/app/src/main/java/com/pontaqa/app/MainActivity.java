9eb55801d664d9a647ff7463038a18546577a2db
Thiago Bastani <thiagobastani.t@gmail.com>
2026-05-26 00:51:47 -0300

    feat: update dependencies and add new assets
    - Added Capacitor dependencies to package.json for Android support.
    - Created a new icon.svg file for the application branding.
    - Created a new splash.svg file for the application splash screen.

---
+++ android/app/src/main/java/com/pontaqa/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.pontaqa.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.pontaqa.app',
+  appName: 'Ponta QA',
+  webDir: 'www'
+};
+
+export default config;
